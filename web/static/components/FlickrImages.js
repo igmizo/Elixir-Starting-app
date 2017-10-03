ed2c847f68ece779b46b16aef84661b9f5aa7701
igmizo <gmz@gmz.lv>
2017-10-04 01:05:14 +0300

    Cleaned

--- web/static/components/FlickrImages.js
+++ web/static/components/FlickrImages.js
@@ -2,8 +2,16 @@ import React from 'react';
 import SearchByAuthor from '../components/SearchByAuthor';
 import FoundImages from '../components/FoundImages';
 
+import axios from 'axios';
 
 class FlickrImages extends React.Component {
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts/1")
+      .then(res => {
+        console.log(res);
+        // this.setState({data})
+      });
+  }
   render() {
     return (
       <div>
